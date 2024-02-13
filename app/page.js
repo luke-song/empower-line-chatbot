c564efb04f1ad99178abd15b299f3c427b30aedd
Luke Song <hyeonminsong7@gmail.com>
2024-02-12 23:17:34 -0800

    :recycle: cleanup, :lipstick: Added Theme and button UI components

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className={styles.buttonContainer}>
+      <button className={styles.button}>Button 1</button>
+      <button className={styles.button}>Button 2</button>
+      <button className={styles.button}>Button 3</button>
+      <button className={styles.button}>Button 4</button>
+      <button className={styles.button}>Button 5</button>
+      <button className={styles.button}>Button 6</button>
+    </div>
+  );
 }
